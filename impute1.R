31c5fc9d60313c8c8edd34a99b52e9bb449c2a99
MayaGutiBan <gubita@icloud.com>
2025-01-22 19:20:30 -0500

    Update impute1.R

--- impute1.R
+++ impute1.R
@@ -3,3 +3,9 @@ ggplot(db, aes(maxEducLevel)) +
   ggtitle("Max Edu  Distribution") +
   theme_classic() +
   theme(plot.title = element_text(size = 18))# Include here your code for your first chosen imputation method
+# calculating the most commun value of maxEducLevel. 
+mode_edu <- as.numeric(names(sort(table(db$maxEducLevel), decreasing = TRUE)[1]))
+
+# Imputing the missing value. 
+db <- db  %>%
+  mutate(maxEducLevel = ifelse(is.na(maxEducLevel) == TRUE, mode_edu , maxEducLevel))
