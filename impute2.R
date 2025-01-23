ad75430ba7f1400c4696de4bdd789f1e2848dcf6
jpgrimaldos <123492640+jpgrimaldos@users.noreply.github.com>
2025-01-22 19:23:37 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -10,3 +10,12 @@ db <- cbind(db, dummy_maxEducLevel)
 
 db %>% select(maxEducLevel,maxEducLevel1, maxEducLevel2,maxEducLevel3 ,maxEducLevel4 ,maxEducLevel5 ,maxEducLevel6, maxEducLevel7 ) %>% head() 
 
+linear_imput_model <- lm(y_salary_m ~ ingtot + sex   + maxEducLevel3 + maxEducLevel4 + maxEducLevel5 + maxEducLevel6 + maxEducLevel7 , data = db)
+summary(linear_imput_model)
+
+db$predicted_y <- predict(linear_imput_model, newdata = db)
+
+db %>% select(y_salary_m, predicted_y, y_salary_m  ) %>% head() 
+
+## drop recently created variables
+db<-  db %>% select(- maxEducLevel1, - maxEducLevel2, - maxEducLevel3, - maxEducLevel4, - maxEducLevel5, - maxEducLevel6, - maxEducLevel7,-predicted_y )
