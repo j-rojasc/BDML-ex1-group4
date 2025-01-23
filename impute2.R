24affdf32a14f00ff831c05a9227ddfd66064bb1
jpgrimaldos <123492640+jpgrimaldos@users.noreply.github.com>
2025-01-22 19:22:09 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -1,1 +1,12 @@
 # Include your code here for you second imputation method
+
+# transform categorical to factor
+
+db$maxEducLevel<- factor(db$maxEducLevel)
+
+dummy_maxEducLevel <- as.data.frame(model.matrix(~ maxEducLevel - 1, data = db)) 
+#db<- db  %>% cbind(dummy_maxEducLevel) 
+db <- cbind(db, dummy_maxEducLevel)
+
+db %>% select(maxEducLevel,maxEducLevel1, maxEducLevel2,maxEducLevel3 ,maxEducLevel4 ,maxEducLevel5 ,maxEducLevel6, maxEducLevel7 ) %>% head() 
+
