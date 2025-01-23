3c52685dfd58adc1c51f82d5c0b83305f42113d6
jpgrimaldos <123492640+jpgrimaldos@users.noreply.github.com>
2025-01-22 19:27:45 -0500

    Update impute2.R

--- impute2.R
+++ impute2.R
@@ -17,5 +17,9 @@ db$predicted_y <- predict(linear_imput_model, newdata = db)
 
 db %>% select(y_salary_m, predicted_y, y_salary_m  ) %>% head() 
 
+db<-  db %>%  mutate(y_salary_m = ifelse(is.na(y_salary_m) == TRUE, predicted_y , y_salary_m))
+
+db %>% select(y_salary_m, predicted_y, y_salary_m  ) %>% head() 
+
 ## drop recently created variables
 db<-  db %>% select(- maxEducLevel1, - maxEducLevel2, - maxEducLevel3, - maxEducLevel4, - maxEducLevel5, - maxEducLevel6, - maxEducLevel7,-predicted_y )
