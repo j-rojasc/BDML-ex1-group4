917be0e8d3a9af9601d88539c16c2598eac11957
MayaGutiBan <gubita@icloud.com>
2025-01-22 19:20:03 -0500

    Update impute1.R

--- impute1.R
+++ impute1.R
@@ -1,1 +1,5 @@
-# Include here your code for your first chosen imputation method
+ggplot(db, aes(maxEducLevel)) +
+  geom_histogram(color = "#000000", fill = "#0099F8") +
+  ggtitle("Max Edu  Distribution") +
+  theme_classic() +
+  theme(plot.title = element_text(size = 18))# Include here your code for your first chosen imputation method
